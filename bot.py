fb40c96b1101b9e8ab048f3f3c121f9736fb5316
guiliheitor-eng <guiliheitor@gmail.com>
2026-03-27 18:27:42 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -18,6 +18,25 @@ async def hello(ctx):
 async def heh(ctx, count_heh = 5):
     await ctx.send("he" * count_heh)
 
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user} (ID: {bot.user.id})')
+    print('------')
+
+@bot.command(description='For when you wanna settle the score some other way')
+async def choose(ctx, *choices: str):
+    """Chooses between multiple choices."""
+    await ctx.send(random.choice(choices))
+
+@bot.group()
+async def cool(ctx):
+    """Says if a user is cool.
+
+    In reality this just checks if a subcommand is being invoked.
+    """
+    if ctx.invoked_subcommand is None:
+        await ctx.send(f'No, {ctx.subcommand_passed} is not cool')
+
 
 @bot.command()
 async def add(ctx, left: int, right: int):
